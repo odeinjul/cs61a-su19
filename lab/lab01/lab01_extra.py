ed17dd2e507147c7a7a648865e1e76f39bf33699
odeinjul <joshua007v@outlook.com>
2021-07-12 17:25:57 +0800

    -_-

---
+++ lab/lab01/lab01_extra.py
@@ -0,0 +1,45 @@
+"""Optional questions for Lab 1"""
+
+# While Loops
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 0)
+    1
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    """
+    time,i = 1,1
+    while (i <= k):
+        time = time * n
+        n = n - 1
+        i = i + 1
+    return time
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    while n > 0:
+        if n % 100 == 88:
+            return True
+        else:
+            n //= 10
+    return False
+    
