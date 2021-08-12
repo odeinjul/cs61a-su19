a64fc71bcabb21edf8236b7a35807d4749747ca1
odeinjul <joshua007v@outlook.com>
2021-08-12 21:52:13 +0800

    Finished lab10

---
+++ lab/lab10/lab10_extra.scm
@@ -0,0 +1,68 @@
+;; Scheme ;;
+
+
+(define lst
+(cons (cons 1 nil) (cons 2 (cons (cons 3 (cons 4 nil))(cons 5 nil)))))
+
+(define (composed f g)
+  'YOUR-CODE-HERE
+  (lambda (x) (f (g x)))
+)
+
+(define (filter-lst f lst)
+  'YOUR-CODE-HERE
+  (cond
+    ((null? lst) nil)
+    ((f (car lst)) (cons (car lst) (filter-lst f (cdr lst))))
+    (else (filter-lst f (cdr lst)))
+  )
+)
+
+(define (remove item lst)
+  'YOUR-CODE-HERE
+  (filter (lambda (x) (not (equal? x item))) lst))
+
+
+;;; Tests
+(remove 3 nil)
+; expect ()
+(remove 3 '(1 3 5))
+; expect (1 5)
+(remove 5 '(5 3 5 5 1 4 5 4))
+; expect (3 1 4 4)
+
+(define (no-repeats s)
+  'YOUR-CODE-HERE
+  (if (null? s)
+    nil
+    (cons
+      (car s)
+      (no-repeats (remove (car s) (cdr s))))))
+
+(define (substitute s old new)
+  'YOUR-CODE-HERE
+  (if (null? s)
+    nil
+    (cons
+      (if (not (pair? (car s)))
+        (if (equal? (car s) old)
+          new
+          (car s))
+        (substitute (car s) old new))
+      (substitute (cdr s) old new))))
+
+
+; https://github.com/sgalal/cs61a/blob/master/Labs/lab10/lab10_extra.scm#L41
+(define (sub-all s olds news)
+  'YOUR-CODE-HERE
+  (define (zip xs ys)
+    (if (or (null? xs) (null? ys))
+      nil
+      (cons
+        (list (car xs) (car ys))
+        (zip (cdr xs) (cdr ys)))))
+  ((reduce
+    composed
+    (map
+      (lambda (x-y) (lambda (s) (substitute s (car x-y) (car (cdr x-y)))))
+      (zip olds news))) s))
