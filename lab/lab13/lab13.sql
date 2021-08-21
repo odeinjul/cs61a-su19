2e24e2695c8e1f040de8d7643c25c35303b643fb
Odeinjul <odeinjul@gmail.com>
2021-08-21 22:44:18 +0800

    Finished lab13

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -1,13 +1,13 @@
 .read su19data.sql
 
 CREATE TABLE obedience AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, instructor FROM students;
 
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest FROM students WHERE smallest > 2 ORDER BY smallest LIMIT 20;
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color FROM students AS a ,students AS b WHERE a.pet = b.pet AND a.song = b.song AND a.time < b.time ;
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest FROM students GROUP BY smallest HAVING count(*) = 1 ORDER BY smallest;
--- lab/lab13/lab13_extra.sql
+++ lab/lab13/lab13_extra.sql
@@ -1,12 +1,12 @@
 .read lab13.sql
 
 CREATE TABLE su19favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(pet) FROM students GROUP BY pet ORDER BY count(pet) DESC LIMIT 10;
 
 
 CREATE TABLE su19dog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(pet) FROM students GROUP BY pet ORDER BY count(pet) DESC LIMIT 1;
 
 
 CREATE TABLE obedienceimages AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, instructor, count(instructor) FROM students WHERE seven = "7" GROUP BY instructor;
